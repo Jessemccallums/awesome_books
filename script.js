43deb8e3f6ac996b1867cd336b7e11977b7d175e
bengigenc <genc.bengi@gmail.com>
2022-06-27 18:08:00 +0300

    Set and display local storage data

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-
-const aweosomeBooks = [
-    {
-        title: "Lorem",
-        author: "author1",
-    },
-    {
-        title: "Dummy",
-        author: "Author2"
-    },
-    {
-        title: "Jitterburg Perfume",
-        author: "Tom Robbins"
-    }
-]
-
-
 const titleEl = document.querySelector('#title');
 const authorEl = document.querySelector('#author');
 const addBtn = document.querySelector('#add-btn');
 
 addBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    
-    const newBook = {
-        title: titleEl.value,
-        author: authorEl.value
-    };
-
-    aweosomeBooks.push(newBook);
-
-    console.log(aweosomeBooks);
+    let awesomeBooks = [];
+    console.log(awesomeBooks);
+    if (titleEl.value !== '' && authorEl.value !== '') {
+        const newBook = {
+            title: titleEl.value,
+            author: authorEl.value,
+            id: Date.now().toString(),
+        };
+        awesomeBooks.push(newBook);
+        console.log(awesomeBooks);
+        awesomeBooks = awesomeBooks.concat(JSON.parse(localStorage.getItem('awesomeBooks')));
+        localStorage.setItem('awesomeBooks', JSON.stringify(awesomeBooks));
+        console.log(awesomeBooks);
+        window.location.reload();
+    } else {
+        alert("invalid input value");
+    }
 })
+
 
 const displaySection = document.querySelector('#display');
 
 function display() {
-    aweosomeBooks.forEach((book) => {
+    let awesomeBooks = [];
+    awesomeBooks = JSON.parse(localStorage.getItem('awesomeBooks'));
+
+
+    awesomeBooks.forEach((book) => {
     displaySection.innerHTML += `
       <p>${book.title}</p>
       <p>${book.author}</p>
