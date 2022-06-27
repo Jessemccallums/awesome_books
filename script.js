8a30ae4b9b12309161c70375da0f6ac9b04b9d3c
bengigenc <genc.bengi@gmail.com>
2022-06-27 15:44:19 +0300

    Create a function to add a new book

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ const aweosomeBooks = [
     }
 ]
 
+const titleEl = document.querySelector('#title');
+const authorEl = document.querySelector('#author');
+const addBtn = document.querySelector('#add-btn');
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    const newBook = {
+        title: titleEl.value,
+        author: authorEl.value
+    };
+
+    aweosomeBooks.push(newBook);
+
+    console.log(aweosomeBooks);
+})
