9159be6e575900efaf3e4a470a0649caffeea069
bengigenc <genc.bengi@gmail.com>
2022-06-27 18:15:37 +0300

    Fix linter issues

--- script.js
+++ script.js
@@ -3,42 +3,40 @@ const authorEl = document.querySelector('#author');
 const addBtn = document.querySelector('#add-btn');
 
 addBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    let awesomeBooks = [];
+  e.preventDefault();
+  let awesomeBooks = [];
+  console.log(awesomeBooks);
+  if (titleEl.value !== '' && authorEl.value !== '') {
+    const newBook = {
+      title: titleEl.value,
+      author: authorEl.value,
+      id: Date.now().toString(),
+    };
+    awesomeBooks.push(newBook);
     console.log(awesomeBooks);
-    if (titleEl.value !== '' && authorEl.value !== '') {
-        const newBook = {
-            title: titleEl.value,
-            author: authorEl.value,
-            id: Date.now().toString(),
-        };
-        awesomeBooks.push(newBook);
-        console.log(awesomeBooks);
-        awesomeBooks = awesomeBooks.concat(JSON.parse(localStorage.getItem('awesomeBooks')));
-        localStorage.setItem('awesomeBooks', JSON.stringify(awesomeBooks));
-        console.log(awesomeBooks);
-        window.location.reload();
-    } else {
-        alert("invalid input value");
-    }
-})
-
+    awesomeBooks = awesomeBooks.concat(JSON.parse(localStorage.getItem('awesomeBooks')));
+    localStorage.setItem('awesomeBooks', JSON.stringify(awesomeBooks));
+    console.log(awesomeBooks);
+    window.location.reload();
+  } else {
+    alert('invalid input value');
+  }
+});
 
 const displaySection = document.querySelector('#display');
 
 function display() {
-    let awesomeBooks = [];
-    awesomeBooks = JSON.parse(localStorage.getItem('awesomeBooks'));
+  let awesomeBooks = [];
+  awesomeBooks = JSON.parse(localStorage.getItem('awesomeBooks'));
 
-
-    awesomeBooks.forEach((book) => {
+  awesomeBooks.forEach((book) => {
     displaySection.innerHTML += `
       <p>${book.title}</p>
       <p>${book.author}</p>
       <button class="remove">Remove</button>
       <hr>
     `;
-})
-};
+  });
+}
 
 display();
