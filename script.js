9223b68d28a2a1d431fa10edc9fec0b69fefaaa3
Jessemccallums <jessemccallums2001@gmail.com>
2022-06-27 11:55:54 -0400

    Update JS file

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const titleEl = document.querySelector('#title');
 const authorEl = document.querySelector('#author');
 const addBtn = document.querySelector('#add-btn');
+const removeBtn = document.querySelectorAll(".remove");
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -31,12 +32,28 @@ function display() {
 
   awesomeBooks.forEach((book) => {
     displaySection.innerHTML += `
+      <div>
       <p>${book.title}</p>
       <p>${book.author}</p>
-      <button class="remove">Remove</button>
+      <button class="remove" id="${book.id}">Remove</button>
       <hr>
+      </div>
     `;
   });
 }
 
 display();
+
+function deleteBtn(e){
+    
+    let awesomeBooks = [];
+    awesomeBooks = awesomeBooks.concat(JSON.parse(localStorage.getItem('awesomeBooks')));
+    
+    if (!e.target.classList.contains("remove")){
+        return;
+    }
+    
+    const boton = e.target;
+    boton.closest("div").remove();
+}
+
