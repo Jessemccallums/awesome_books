14a8c1a13375745f201dc688a73c7356d8dec471
bengigenc <genc.bengi@gmail.com>
2022-06-27 15:33:38 +0300

    Create a book collection object

---
+++ script.js
@@ -0,0 +1,15 @@
+const aweosomeBooks = [
+    {
+        title: "Lorem",
+        author: "author1",
+    },
+    {
+        title: "Dummy",
+        author: "Author2"
+    },
+    {
+        title: "Jitterburg Perfume",
+        author: "Tom Robbins"
+    }
+]
+
